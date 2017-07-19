c8be6fb699d3eb6b7cfe71f594b805154f634c8b
Nicole Sattler <nicole.sattler@live.com>
2017-07-19 17:36:27 +0000

    Applied functions to open/close input section

--- src/AddRecipe.js
+++ src/AddRecipe.js
@@ -4,10 +4,22 @@ import React from 'react';
 import './App.css';
 
 class AddRecipe extends React.Component {
+  constructor(props){
+    super();
+    this.handleEdit = this.handleEdit.bind(this);
+  }
+  
+  handleEdit(){
+    this.props.submitEdit();
+    this.props.hideEditBox();
+  }
+
+  
   render() {
     $('#save').off();
     
     return (
+      <div>
       <div className="newRecipe">
         
         <input type="text" id="dish" placeholder="Recipe name" value={this.props.title} onChange={this.props.handleTitle} />
@@ -15,8 +27,12 @@ class AddRecipe extends React.Component {
         <input type="text" id="ingredients" placeholder="Ingredients, seperated by commas" value={this.props.ingredients} onChange={this.props.handleChange}/>
         
         <button id="save" onClick={this.props.handleSubmit} >Save</button>
-        <button onClick={this.props.submitEdit}>Save Edit</button>
+        <button onClick={this.handleEdit}>Save Edit</button>
         
+        
+      </div>
+      
+      <button id="addNew" onClick={this.props.showEditBox} ><i className="fa fa-plus"></i></button>
       </div>
     );
   }
